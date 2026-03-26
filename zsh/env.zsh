4052a58d5e45809c7c8da6f14a8be3580af2221f
youki <youkipepper@gmail.com>
2026-03-26 19:52:28 +0800

    update: 2026-03-26 19:52:28

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -6,7 +6,7 @@ export PATH="$HOME/bin:$PATH"
 export EDITOR="nvim"
 
 # Load private overrides
-PRIVATE_ENV="$HOME/.config/dotfiles/env.local.zsh"
+PRIVATE_ENV="$HOME/dotfiles/zsh/env.local.zsh"
 if [ -f "$PRIVATE_ENV" ]; then
     . "$PRIVATE_ENV"
 fi
